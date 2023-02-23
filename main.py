a98234b1bdc7c30a15cab06cc8f47bc551995e8c
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-02-23 23:34:30 +0100

    save in files

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import aggregate
+from datetime import datetime
 import plot
 
 if __name__ == '__main__':
     chair_data = aggregate.get_chair_data()
-    
-    with open('data.json', 'w') as f:
+
+    with open(f'{datetime.now().isoformat()}-stef-chair.json', 'w') as f:
         f.write(aggregate.chair_data_to_json(chair_data))
 
     plot.plot_by_chair(chair_data)
