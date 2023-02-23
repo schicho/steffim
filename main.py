33a120b8c0cfd6fb51152792bc82f9c3b19dc81a
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-02-23 16:56:14 +0100

    plot!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import aggregate
+import plot
 
 if __name__ == '__main__':
-    print(aggregate.get_chair_data())
+    chair_data = aggregate.get_chair_data()
+    plot.plot_by_chair(chair_data)
---
+++ plot.py
@@ -0,0 +1,14 @@
+from aggregate import UniversityChair
+import matplotlib.pyplot as plt
+
+def plot_by_chair(chair_data):
+    # leave out chairs with no stef
+    chair_data = [chair for chair in chair_data if chair.stef_stats()[1] > 0]
+
+    # get the names and number of stef
+    names = [chair.stef_stats()[0]  for chair in chair_data]
+    stef = [chair.stef_stats()[1] for chair in chair_data]
+
+    # plot the data
+    plt.bar(names, stef)
+    plt.show()
