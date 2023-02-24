e9c8c0f0ecbf7663bd9310a7eae87ea7e5224f59
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-02-24 13:13:32 +0100

    historic data

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import plot
 if __name__ == '__main__':
     chair_data = aggregate.get_chair_data()
 
-    with open(f'{datetime.now().isoformat()}-stef-chair.json', 'w') as f:
+    with open(f'historic/{datetime.now().isoformat()}-stef-chair.json', 'w') as f:
         f.write(aggregate.chair_data_to_json(chair_data))
 
     plot.plot_by_chair(chair_data)
