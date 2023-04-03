3ebe7ed3c4ef2a78579674b5526a515259bb4fe2
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2023-04-03 21:09:09 +0200

    move try outside of main into script

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import os
 import asyncio
 
 async def main():
-    try:
-        chair_data = await aggregate.get_chair_data()
-
-        with open(f'historic/{datetime.now().isoformat()}-stef-fim.json', 'w') as f:
-            f.write(aggregate.chair_data_to_json(chair_data))
-
-        plot.plot_by_chair(chair_data)
-        plot.plot_over_time()
-    except Exception as e:
-        logging.error(f'process failed: {e}')
+    chair_data = await aggregate.get_chair_data()
+    with open(f'historic/{datetime.now().isoformat()}-stef-fim.json', 'w') as f:
+        f.write(aggregate.chair_data_to_json(chair_data))
+    plot.plot_by_chair(chair_data)
+    plot.plot_over_time()
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s [%(filename)s %(lineno)d]: %(message)s')
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logging.error(f'process failed: {e}')
+        os._exit(1)
