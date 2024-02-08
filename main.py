3ced4fd598ce84740309716a1bb99f830df84f3a
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2024-02-08 21:06:22 +0100

    new json file naming with only the iso date

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import plot
 
 async def main():
     chair_data = await aggregate.get_chair_data()
-    with open(f"historic/{datetime.now().isoformat()}-stef-fim.json", "w") as f:
+
+    calendar_iso_date = datetime.now().strftime("%Y-%m-%d")
+    with open(f"historic/{calendar_iso_date}-stef-fim.json", "w") as f:
         f.write(aggregate.chair_data_to_json(chair_data))
     plot.plot_by_chair(chair_data)
     plot.plot_over_time()
